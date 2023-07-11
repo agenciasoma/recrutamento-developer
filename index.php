41bf9203ecd62204af6e22c834d65682ed807f40
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-07-11 13:08:24 -0300

    feat: query posts

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php get_header();
 $botao = 'Continue lendo';
 ?>
+<?php
+	global $wpdb;
+	// $posts = $wpdb->get_results("SELECT * FROM egddy_posts WHERE post_type = 'BLOG' AND post_date > '2019-09-10 00:00:00' AND post_date < '2019-09-20 00:00:00' AND post_status = 'free'");
+	$posts = $wpdb->get_results("SELECT * FROM egddy_posts WHERE post_date > '2019-09-10 00:00:00' AND post_date < '2019-09-20 00:00:00'");
+?>
 <div class="container-page">
 	<div class="header-page" style="background: url('<?php bloginfo('template_url'); ?>/src/images/bg-blog.jpg') no-repeat center center;background-size: cover;">
 		<div class="center">
@@ -12,100 +17,71 @@ $botao = 'Continue lendo';
 		<div class="center-small">
 			<div class="top-itens">
 				<div class="center-small">
-					<div class="col">
-						<div class="item">
-							<a href="./blanditiis-velit-esse-cumque-minima-consequatur/" title="Blanditiis velit esse cumque minima consequatur">
-								<img src="./wp-content/themes/somadev-theme-master/src/images/no-thumb-post-big.jpg">
-							</a>
-							<div class="wrapper">
-								<div class="category">
-									Blog
-								</div>
-								<div class="text">
-									<h2 class="title">
-										<a href="./blanditiis-velit-esse-cumque-minima-consequatur/" title="Blanditiis velit esse cumque minima consequatur">Blanditiis velit esse cumque minima consequatur</a>
-									</h2>
-									<div class="text-mobile">
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam delectus sed molestias deserunt. Assumenda illum eum placeat cum repellat quisquam cumque doloribus distinctio, dol...</p>
+					<?php 
+						foreach($posts as $post) {
+					?>
+						<div class="col">
+							<div class="item item-small">
+								<a href="#" title=<?php $post->post_title ?>>
+									<img src="<?php bloginfo('template_url'); ?>/src/images/no-thumb-post-small.jpg" alt="image">
+								</a>
+								<div class="wrapper">
+									<div class="category">
+										<?php
+											$post_type = ucfirst(substr($post->post_type, 0, 1));
+											echo $post_type . substr($post->post_type, 1);
+										?>
 									</div>
-									<a href="./blanditiis-velit-esse-cumque-minima-consequatur/" class="link-more" title="Continue lendo Blanditiis velit esse cumque minima consequatur">Continue lendo</a>
+									<div class="text">
+										<h2 class="title">
+											<a href="./blanditiis-velit-esse-cumque-minima-consequatur/" title=<?php $post->post_title ?>><?php echo $post->post_title ?></a>
+										</h2>
+										<div class="text-mobile">
+											<p><?php 
+												$excerpt = $post->post_content;
+												echo substr($excerpt, 0, 100);
+											?></p>
+										</div>
+										<a href="./blanditiis-velit-esse-cumque-minima-consequatur/" class="link-more" title=<?php "Continue lendo " . $post->post_title ?>>Continue lendo</a>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-					<div class="col">
-						<div class="item item-small">
-							<a href="./atque-enim-consectetur-quod/" title="Atque enim consectetur quod">
-								<img src="./wp-content/themes/somadev-theme-master/src/images/no-thumb-post-small.jpg">
-							</a>
-							<div class="wrapper">
-								<div class="category">
-									Blog
-								</div>
-								<div class="text">
-									<h2 class="title">
-										<a href="./atque-enim-consectetur-quod/" title="Atque enim consectetur quod">Atque enim consectetur quod</a>
-									</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea non dolorem, a consequatur debitis voluptas accusantium veritatis. Itaque officia veniam maiores commodi, atque enim...</p>
-									<a href="./atque-enim-consectetur-quod/" class="link-more" title="Continue lendo Atque enim consectetur quod">Continue lendo</a>
-								</div>
-							</div>
-						</div>
-						<div class="item item-small">
-							<a href="./lorem-ipsum-dolor-sit-amet/" title="Lorem ipsum dolor sit amet">
-								<img src="./wp-content/themes/somadev-theme-master/src/images/no-thumb-post-small.jpg">
-							</a>
-							<div class="wrapper">
-								<div class="category">
-									Blog
-								</div>
-								<div class="text">
-									<h2 class="title">
-										<a href="./lorem-ipsum-dolor-sit-amet/" title="Lorem ipsum dolor sit amet">Lorem ipsum dolor sit amet</a>
-									</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea non dolorem, a consequatur debitis voluptas accusantium veritatis. Itaque officia veniam maiores commodi, atque enim...</p>
-									<a href="./lorem-ipsum-dolor-sit-amet/" class="link-more" title="Continue lendo Lorem ipsum dolor sit amet">Continue lendo</a>
-								</div>
-							</div>
-						</div>
-					</div>
+					<?php
+						}
+					?>
 				</div>
 			</div>
 			<div class="itens-post">
-				<div class="item">
-					<div class="wrapper">
-						<div class="image">
-							<img src="./wp-content/themes/somadev-theme-master/src/images/no-thumb-big.jpg">
-						</div>
-						<div class="text">
-							<h2>
-								<a href="#" title="novo teste de conteúdo">novo teste de conteúdo</a>
-							</h2>
-							<span class="icons-post icon-date">
-								<i class="icon-calendar"></i>18.07.2019
-							</span>
-							<a href="#" class="icons-post icon-coment-count">
-								<i class="icon-comment-alt"></i>0
-							</a>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci accusantium repudiandae aperiam dolorem error consequatur harum nam distinctio, voluptates mollitia excepturi fac</p>
-							<a class="link-more" href="#" title="novo teste de conteúdo">Continue lendo</a>
-						</div>
-					</div>
-				</div>
-				<div class="item">
-					<div class="wrapper">
-						<div class="image">
-							<img src="./wp-content/themes/somadev-theme-master/src/images/no-thumb-big.jpg">
-						</div>
-						<div class="text">
-							<h2><a href="#" title="Dolor impedit soluta perspiciatis fugiat">Dolor impedit soluta perspiciatis fugiat</a></h2>
-							<span class="icons-post icon-date"><i class="icon-calendar"></i>17.07.2019</span>
-							<a href="#" class="icons-post icon-coment-count"><i class="icon-comment-alt"></i>0</a>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam delectus sed molestias deserunt. Assumenda illum eum placeat cum repellat quisquam cumque doloribus distinctio, dolor </p>
-							<a class="link-more" href="#" title="Dolor impedit soluta perspiciatis fugiat">Continue lendo</a>
+				<?php 
+					foreach($posts as $post) {
+				?>
+					<div class="item">
+						<div class="wrapper">
+							<div class="image">
+								<img src="<?php bloginfo('template_url'); ?>/src/images/no-thumb-post-small.jpg" alt="image">
+							</div>
+							<div class="text">
+								<h2>
+									<a href="#" title=<?php $post->post_title ?>><?php echo $post->post_title ?></a>
+								</h2>
+								<span class="icons-post icon-date">
+									<i class="icon-calendar"></i><?php echo date("d/m/Y", strtotime($post->post_date)) ?>
+								</span>
+								<a href="#" class="icons-post icon-coment-count">
+									<i class="icon-comment-alt"></i><?php echo $post->comment_count ?>
+								</a>
+								<p><?php 
+									$excerpt = $post->post_content;
+									echo substr($excerpt, 0, 100);
+								?></p>
+								<a class="link-more" href="#" title=<?php "Continue lendo " . $post->post_title ?>>Continue lendo</a>
+							</div>
 						</div>
 					</div>
-				</div>
+				<?php
+					}
+				?>
 			</div>
 		</div>
 	</div>
